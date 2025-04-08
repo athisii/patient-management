786db38d549a571be5cb100e84b933add9561248
athisii <athisiiekhe@gmail.com>
2025-04-09 00:15:38 +0530

    add CfnHealthCheck

--- infrastructure/src/main/java/com/athisii/stack/LocalStack.java
+++ infrastructure/src/main/java/com/athisii/stack/LocalStack.java
@@ -1,6 +1,12 @@
 package com.athisii.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
+import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
 /**
  * @author athisii ekhe
@@ -9,9 +15,48 @@ import software.amazon.awscdk.*;
  */
 
 public class LocalStack extends Stack {
+    private final Vpc vpc;
 
     public LocalStack(final App scope, String id, StackProps props) {
         super(scope, id, props);
+        this.vpc = createVpc();
+
+        DatabaseInstance authServiceDb = createDatabase("AuthServiceDb", "auth-service-db");
+        DatabaseInstance patientServiceDb = createDatabase("PatientServiceDb", "patient-service-db");
+        CfnHealthCheck authDbHealCheck = createDbHealthCheck(authServiceDb, "AuthServiceDbHealthCheck");
+        CfnHealthCheck patientDbbbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDbHealthCheck");
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder.create(this, "PatientManagementVpc")
+                .vpcName("PatientManagementVpc")
+                .maxAzs(2)
+                .build();
+    }
+
+    private DatabaseInstance createDatabase(String id, String dbName) {
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder().version(PostgresEngineVersion.VER_17_4).build()))
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
+    private CfnHealthCheck createDbHealthCheck(DatabaseInstance databaseInstance, String id) {
+        return CfnHealthCheck.Builder.create(this, id)
+                .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+                        .type("TCP")
+                        .port(Token.asNumber(databaseInstance.getDbInstanceEndpointPort()))
+                        .ipAddress(databaseInstance.getDbInstanceEndpointAddress())
+                        .requestInterval(30)
+                        .failureThreshold(3)
+                        .build())
+                .build();
     }
 
     public static void main(String[] args) {
